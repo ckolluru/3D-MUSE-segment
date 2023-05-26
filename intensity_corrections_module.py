058f018a46cccafbf5e56ab2732c6da00fcb5cdd
Chaitanya <kolluru.chaitanya@gmail.com>
2023-05-26 03:26:37 -0400

    Basic stuff.

---
+++ intensity_corrections_module.py
@@ -0,0 +1,88 @@
+import numpy as np
+import tifffile as tiffio
+import glob
+import os
+from tqdm import tqdm
+from PIL import Image
+import shutil
+import math
+
+'''---------------------------------------
+Summary: Bleach and gamma correction
+
+This script takes a folder name that was created from the previous scripts.
+Example: sample_name\\Red channel
+Verify that the first image in the stack looks reasonable if doing bleach correction or gamma correction.
+If not, delete those slices.
+------------------------------------------'''
+
+'''---------------------------------------
+Inputs
+------------------------------------------'''
+sample_name = 'mid-cervical-sr001-r'
+sub_folder  = 'r0_g1_b0'
+
+sample_folder = r'C:\\Users\\cxk340\\Desktop\\' + sample_name + '\\'
+save_folder = r'Individual image slices\\'
+
+# If you want to do bleach correction
+bleach_correction = True
+
+# If you want to do gamma correction
+gamma_correction = True
+gamma_value = 0.54
+
+gamma_bleach_correction_folder = r'Gamma bleach corrected slices\\'
+
+'''---------------------------------------
+Inputs
+------------------------------------------'''
+
+
+'''---------------------------------------
+Algorithm
+------------------------------------------'''
+stacks_folder = sample_folder + sub_folder + '\\'
+
+# Remove save folder if it exists
+if os.path.exists(stacks_folder + gamma_bleach_correction_folder):
+    shutil.rmtree(stacks_folder + gamma_bleach_correction_folder)
+
+os.makedirs(stacks_folder + gamma_bleach_correction_folder)
+
+# Bytes per image
+bytes_per_image = 12009644
+
+# Example image size
+example_width = 4000
+example_height = 3000
+
+# List of image files
+image_filelist = glob.glob(stacks_folder + save_folder + '*.png')
+
+for k in tqdm(np.arange(len(image_filelist))):
+
+    image = Image.open(image_filelist[k])
+    image = np.array(image)
+
+    # Bleach correction, simple ratio method, background assumed to be 0.
+    if bleach_correction:
+        if k == 0:
+            mean_first_slice = np.mean(image)
+        else:
+            mean_current_slice = np.mean(image)
+            image = mean_first_slice * image / (mean_current_slice)
+            image = np.floor(np.clip(image, 0, 255)) 
+            image = image.astype(np.uint8)
+
+    if gamma_correction:
+        # do gamma correction
+        image = (np.power(image/255, gamma_value)*255).clip(0,255).astype(np.uint8)
+
+    if k != 0:
+        pil_image = Image.fromarray(image)
+        pil_image.save(stacks_folder + gamma_bleach_correction_folder + 'Image_' + str(k).zfill(5) + '.png')
+
+'''---------------------------------------
+Algorithm
+------------------------------------------'''
--- main.py
+++ main.py
@@ -1,42 +1,106 @@
-from PyQt5.QtWidgets import*
+from PyQt5.QtWidgets import *
 from PyQt5.uic import loadUi
 
-from matplotlib.backends.backend_qt5agg import (NavigationToolbar2QT as NavigationToolbar)
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+from os.path import expanduser
 
-import numpy as np
-import random
-     
-class MPLWidget(QMainWindow):
-    
-    def __init__(self):
-        
-        QMainWindow.__init__(self)
+import load_intermediate_slices
+	 
+class SBFPreprocess(QMainWindow):
+	
+	def __init__(self):
+		
+		QMainWindow.__init__(self)
 
-        loadUi("mainwindow.ui",self)
-        self.setWindowTitle("SBFpreprocess v1.0")
-        self.addToolBar(NavigationToolbar(self.MatplotlibWidget.canvas, self))
+		loadUi("mainwindow.ui",self)
+		self.setWindowTitle("SBFpreprocess v1.0")
+		self.addToolBar(NavigationToolbar(self.MatplotlibWidget.canvas, self))
+  
+		self.inputFolder = None
+		self.outputFolder = None
+		self.subsetVolume = None
 
+	def openFolder(self):
+		title = "Open the root folder with the dataset, from Micromanager"
+		flags = QFileDialog.ShowDirsOnly
+		self.inputFolder = QFileDialog.getExistingDirectory(self,
+															title,
+															expanduser("."),
+															flags)
 
-    def update_graph(self):
+		if self.inputFolder == '':
+			self.inputFolder = None
+			return
+		
+		self.statusBar().showMessage('Image folder location saved', 2000)
 
-        fs = 500
-        f = random.randint(1, 100)
-        ts = 1/fs
-        length_of_signal = 100
-        t = np.linspace(0,1,length_of_signal)
-        
-        cosinus_signal = np.cos(2*np.pi*f*t)
-        sinus_signal = np.sin(2*np.pi*f*t)
+	def selectOutputFolder(self):
+		title = "Open the folder to save the dataset"
+		flags = QFileDialog.ShowDirsOnly
+		self.outputFolder = QFileDialog.getExistingDirectory(self,
+															title,
+															expanduser("."),
+															flags)
 
-        self.MatplotlibWidget.canvas.axes.clear()
-        self.MatplotlibWidget.canvas.axes.plot(t, cosinus_signal)
-        self.MatplotlibWidget.canvas.axes.plot(t, sinus_signal)
-        self.MatplotlibWidget.canvas.axes.legend(('cosinus', 'sinus'),loc='upper right')
-        self.MatplotlibWidget.canvas.axes.set_title('Cosinus - Sinus Signal')
-        self.MatplotlibWidget.canvas.draw()
-        
+		if self.outputFolder == '':
+			self.outputFolder = None
+			return
+		
+		self.outputFolderLocationLineEdit.setText(str(self.outputFolder))
+		self.statusBar().showMessage('Output folder location saved', 2000)
+  	
+	def processSubsetForVisualization(self):
+	 
+		# Check whether an input directory was provided
+		if self.inputFolder == None:
+			msgBox = QMessageBox()
+			msgBox.setText("Input image directory was not provided, please set it first.")
+			msgBox.setWindowTitle("Error")
+			msgBox.exec()
+			return
 
-app = QApplication([])
-window = MPLWidget()
-window.show()
-app.exec_()
+		# Get information from the UI
+		self.processEvery = int(self.processEveryLineEdit.text())
+		self.tilesX = int(self.tilesXLineEdit.text())
+		self.tilesY = int(self.tilesYLineEdit.text())
+		self.correctIntensityVariationFlag = self.correctIntensityVariationsCheckbox.isChecked()
+		self.gamma = float(self.gammaValueLineEdit.text())
+		self.registerSlicesFlag = self.registerSlicesCheckBox.isChecked()
+  
+		# Load intermediate slices
+		self.subsetVolume = load_intermediate_slices.load()
+
+		# Update slider limits
+		self.xySlider.SetMaximum(self.subsetVolume.shape[2])
+  
+	def changeViewingSlice(self, value):
+	 
+		self.MatplotlibWidget.canvas.axes.clear()
+		self.MatplotlibWidget.canvas.axes.imshow(self.subsetVolume[:,:,int(value)])
+		self.MatplotlibWidget.canvas.draw()	  
+  
+	def processStack(self):
+		# Check whether an input directory was provided
+		if self.inputFolder == None:
+			msgBox = QMessageBox()
+			msgBox.setText("Input image directory was not provided, please set it first.")
+			msgBox.setWindowTitle("Error")
+			msgBox.exec()
+			return
+
+		# Check whether an output directory was provided
+		if self.outputFolder == None:
+			msgBox = QMessageBox()
+			msgBox.setText("Output directory was not provided, please set it first.")
+			msgBox.setWindowTitle("Error")
+			msgBox.exec()
+			return		
+
+	def fileExit(self):
+		app.quit()
+  
+if __name__ == '__main__':
+	app = QApplication([])
+	window = SBFPreprocess()
+	window.show()
+	app.exec_()
