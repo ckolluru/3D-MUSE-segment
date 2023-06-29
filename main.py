3259b53f591e017f549bd5dca2b1e3cd908c2a2e
Chaitanya <kolluru.chaitanya@gmail.com>
2023-06-28 23:47:37 -0400

    UI initial setup

--- main.py
+++ main.py
@@ -1,123 +1,82 @@
-from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import*
 from PyQt5.uic import loadUi
 
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
-from os.path import expanduser
 
-import load_intermediate_slices
-import intensity_corrections_module
-import stitching_module
-import register_module
-	 
-class SBFPreprocess(QMainWindow):
+import zarr
+import os
+
+class MainWindow(QMainWindow):
 	
 	def __init__(self):
 		
 		QMainWindow.__init__(self)
 
 		loadUi("mainwindow.ui",self)
-		self.setWindowTitle("SBFpreprocess v1.0")
-		self.addToolBar(NavigationToolbar(self.MatplotlibWidget.canvas, self))
-  
-		self.inputFolder = None
-		self.outputFolder = None
-		self.subsetVolume = None
 
-	def openFolder(self):
-		title = "Open the root folder with the dataset, from Micromanager"
+		self.addToolBar(NavigationToolbar(self.Mplwidget.canvas, self))
+		self.Mplwidget.canvas.axes.axis('off')
+		
+		self.intermediateSlices = None
+		self.zarr_file = None
+
+
+	def updatePlotWindow(self, image, overlay = None):
+
+		self.Mplwidget.canvas.axes.clear()
+		self.Mplwidget.canvas.axes.imshow(image)
+		self.Mplwidget.canvas.draw()
+		
+		# Overlay is the segmentation masks
+		# if overlay is not None:
+		# ...
+	  
+	def openDataset(self):
+
+		title = "Open Zarr dataset directory"
 		flags = QFileDialog.ShowDirsOnly
-		self.inputFolder = QFileDialog.getExistingDirectory(self,
-															title,
-															expanduser("."),
-															flags)
+		self.zarr_file = QFileDialog.getExistingDirectory(self,
+														title,
+														os.path.expanduser("."),
+														flags)
 
-		if self.inputFolder == '':
-			self.inputFolder = None
+		if self.zarr_file == '':
+			self.zarr_file = None
 			return
 		
-		self.statusBar().showMessage('Image folder location saved', 2000)
-
-	def selectOutputFolder(self):
-		title = "Open the folder to save the dataset"
-		flags = QFileDialog.ShowDirsOnly
-		self.outputFolder = QFileDialog.getExistingDirectory(self,
-															title,
-															expanduser("."),
-															flags)
-
-		if self.outputFolder == '':
-			self.outputFolder = None
-			return
+		# Get the spacing between slices to load in via a dialog box
+		# dialog = DatasetInfoDialogBox()
 		
-		self.outputFolderLocationLineEdit.setText(str(self.outputFolder))
-		self.statusBar().showMessage('Output folder location saved', 2000)
-  	
-	def processSubsetForVisualization(self):
-	 
-		# Check whether an input directory was provided
-		if self.inputFolder == None:
-			msgBox = QMessageBox()
-			msgBox.setText("Input image directory was not provided, please set it first.")
-			msgBox.setWindowTitle("Error")
-			msgBox.exec()
-			return
-
-		# Get information from the UI
-		self.processEvery = int(self.processEveryLineEdit.text())
-		self.tilesX = int(self.tilesXLineEdit.text())
-		self.tilesY = int(self.tilesYLineEdit.text())
-		self.correctIntensityVariationFlag = self.correctIntensityVariationsCheckbox.isChecked()
-		self.gamma = float(self.gammaValueLineEdit.text())
-		self.registerSlicesFlag = self.registerSlicesCheckBox.isChecked()
-  
-		# Load intermediate slices 
-		# Output is a list of 2D numpy arrays - [tile_1_slice_1, tile1_slice100... tileN_slice1, tileN_slice100,...]
-		self.subsetVolume = load_intermediate_slices.load(self.inputFolder, self.processEvery, self.tilesX, self.tilesY)
-  
-		# Intensity corrections
-		# Output is a list of 2D numpy arrays - [tile_1_slice_1, tile1_slice100... tileN_slice1, tileN_slice100,...]
-		self.subsetVolume = intensity_corrections_module.correct(self.subsetVolume, self.correctIntensityVariationFlag, self.gamma)
-  
-		# Stitch slices
-		# Output is in a list format - single element, stitched output
-		if self.tilesX != 1 and self.tilesY != 1:
-			self.subsetVolume = stitching_module.stitch(self.subsetVolume)
-
-			# Register slices
-			if self.registerSlicesFlag:
-				self.subsetVolume = register_module.register(self.subsetVolume)
+		# if dialog.exec_() == QDialog.Accepted:
+		# 	z_step = dialog.get_z_spacing()
    
-		# Update slider limits
-		self.xySlider.SetMaximum(self.subsetVolume.shape[2])
-  
-	def changeViewingSlice(self, value):
-	 
-		self.MatplotlibWidget.canvas.axes.clear()
-		self.MatplotlibWidget.canvas.axes.imshow(self.subsetVolume[:,:,int(value)])
-		self.MatplotlibWidget.canvas.draw()	  
-  
-	def processStack(self):
-		# Check whether an input directory was provided
-		if self.inputFolder == None:
-			msgBox = QMessageBox()
-			msgBox.setText("Input image directory was not provided, please set it first.")
-			msgBox.setWindowTitle("Error")
-			msgBox.exec()
-			return
+		# dataset = self.zarr_file["muse"]
+		# self.intermediateSlices = np.array(dataset[:z_step:, :, :])
+		return
+	
+	def saveModelPredictions(self):
+		
+		return
+	
+	def saveTrainedModel(self):
+		
+		return
+	
+	def saveUserAnnotations(self):
+		
+		return
+	
+	def saveUserCorrections(self):
+		
+		return
 
-		# Check whether an output directory was provided
-		if self.outputFolder == None:
-			msgBox = QMessageBox()
-			msgBox.setText("Output directory was not provided, please set it first.")
-			msgBox.setWindowTitle("Error")
-			msgBox.exec()
-			return		
-
-	def fileExit(self):
-		app.quit()
-  
+	def lowEdit(self):
+    
+		lowValue = int(self.lowLineEdit.text())
+		return
+		
 if __name__ == '__main__':
 	app = QApplication([])
-	window = SBFPreprocess()
+	window = MainWindow()
 	window.show()
 	app.exec_()
---
+++ pre-ui/main.py
@@ -0,0 +1,123 @@
+from PyQt5.QtWidgets import *
+from PyQt5.uic import loadUi
+
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+from os.path import expanduser
+
+import load_intermediate_slices
+import intensity_corrections_module
+import stitching_module
+import register_module
+	 
+class SBFPreprocess(QMainWindow):
+	
+	def __init__(self):
+		
+		QMainWindow.__init__(self)
+
+		loadUi("mainwindow.ui",self)
+		self.setWindowTitle("SBFpreprocess v1.0")
+		self.addToolBar(NavigationToolbar(self.MatplotlibWidget.canvas, self))
+  
+		self.inputFolder = None
+		self.outputFolder = None
+		self.subsetVolume = None
+
+	def openFolder(self):
+		title = "Open the root folder with the dataset, from Micromanager"
+		flags = QFileDialog.ShowDirsOnly
+		self.inputFolder = QFileDialog.getExistingDirectory(self,
+															title,
+															expanduser("."),
+															flags)
+
+		if self.inputFolder == '':
+			self.inputFolder = None
+			return
+		
+		self.statusBar().showMessage('Image folder location saved', 2000)
+
+	def selectOutputFolder(self):
+		title = "Open the folder to save the dataset"
+		flags = QFileDialog.ShowDirsOnly
+		self.outputFolder = QFileDialog.getExistingDirectory(self,
+															title,
+															expanduser("."),
+															flags)
+
+		if self.outputFolder == '':
+			self.outputFolder = None
+			return
+		
+		self.outputFolderLocationLineEdit.setText(str(self.outputFolder))
+		self.statusBar().showMessage('Output folder location saved', 2000)
+  	
+	def processSubsetForVisualization(self):
+	 
+		# Check whether an input directory was provided
+		if self.inputFolder == None:
+			msgBox = QMessageBox()
+			msgBox.setText("Input image directory was not provided, please set it first.")
+			msgBox.setWindowTitle("Error")
+			msgBox.exec()
+			return
+
+		# Get information from the UI
+		self.processEvery = int(self.processEveryLineEdit.text())
+		self.tilesX = int(self.tilesXLineEdit.text())
+		self.tilesY = int(self.tilesYLineEdit.text())
+		self.correctIntensityVariationFlag = self.correctIntensityVariationsCheckbox.isChecked()
+		self.gamma = float(self.gammaValueLineEdit.text())
+		self.registerSlicesFlag = self.registerSlicesCheckBox.isChecked()
+  
+		# Load intermediate slices 
+		# Output is a list of 2D numpy arrays - [tile_1_slice_1, tile1_slice100... tileN_slice1, tileN_slice100,...]
+		self.subsetVolume = load_intermediate_slices.load(self.inputFolder, self.processEvery, self.tilesX, self.tilesY)
+  
+		# Intensity corrections
+		# Output is a list of 2D numpy arrays - [tile_1_slice_1, tile1_slice100... tileN_slice1, tileN_slice100,...]
+		self.subsetVolume = intensity_corrections_module.correct(self.subsetVolume, self.correctIntensityVariationFlag, self.gamma)
+  
+		# Stitch slices
+		# Output is in a list format - single element, stitched output
+		if self.tilesX != 1 and self.tilesY != 1:
+			self.subsetVolume = stitching_module.stitch(self.subsetVolume)
+
+			# Register slices
+			if self.registerSlicesFlag:
+				self.subsetVolume = register_module.register(self.subsetVolume)
+   
+		# Update slider limits
+		self.xySlider.SetMaximum(self.subsetVolume.shape[2])
+  
+	def changeViewingSlice(self, value):
+	 
+		self.MatplotlibWidget.canvas.axes.clear()
+		self.MatplotlibWidget.canvas.axes.imshow(self.subsetVolume[:,:,int(value)])
+		self.MatplotlibWidget.canvas.draw()	  
+  
+	def processStack(self):
+		# Check whether an input directory was provided
+		if self.inputFolder == None:
+			msgBox = QMessageBox()
+			msgBox.setText("Input image directory was not provided, please set it first.")
+			msgBox.setWindowTitle("Error")
+			msgBox.exec()
+			return
+
+		# Check whether an output directory was provided
+		if self.outputFolder == None:
+			msgBox = QMessageBox()
+			msgBox.setText("Output directory was not provided, please set it first.")
+			msgBox.setWindowTitle("Error")
+			msgBox.exec()
+			return		
+
+	def fileExit(self):
+		app.quit()
+  
+if __name__ == '__main__':
+	app = QApplication([])
+	window = SBFPreprocess()
+	window.show()
+	app.exec_()
